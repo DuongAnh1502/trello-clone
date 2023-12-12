87f5f2e1568d2dae76772d403b03aca2c3e8debe
duonganh203 <rankill52@gmail.com>
2023-12-12 23:25:25 +0700

    adding local font for marketing page

--- app/(marketing)/page.tsx
+++ app/(marketing)/page.tsx
@@ -1,9 +1,21 @@
+import Link from "next/link";
+import localFont from "next/font/local";
 import { Medal } from "lucide-react";
+
+import { cn } from "@/lib/utils";
+import { Button } from "@/components/ui/button";
+
+const headingFont = localFont({ src: "../../public/fonts/font.woff2" });
 
 const MarketingPage = () => {
     return (
         <div className='flex items-center justify-center flex-col'>
-            <div className='flex items-center justify-center flex-col'>
+            <div
+                className={cn(
+                    "flex items-center justify-center flex-col",
+                    headingFont.className
+                )}
+            >
                 <div className='mb-4 flex items-center border shadow-sm p-4 bg-amber-100 text-amber-700 rounded-full uppercase'>
                     <Medal className='h-6 w-6 mr-2' />
                     No 1 task management
@@ -20,6 +32,9 @@ const MarketingPage = () => {
                 From high rises to the home office, the way your team works is
                 unique - accomplish it all with Taskify
             </div>
+            <Button className='mt-6' size='lg' asChild>
+                <Link href='/sign-up'>Get Taskify for free</Link>
+            </Button>
         </div>
     );
 };
