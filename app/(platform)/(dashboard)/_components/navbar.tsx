bb8cf3e631245bca7cf9e3a20f36830644bb6b5f
duonganh203 <rankill52@gmail.com>
2023-12-15 20:41:58 +0700

    adding organization switcher

--- app/(platform)/(dashboard)/_components/navbar.tsx
+++ app/(platform)/(dashboard)/_components/navbar.tsx
@@ -1,13 +1,34 @@
+import { Plus } from "lucide-react";
+
 import { Logo } from "@/components/logo";
+import { Button } from "@/components/ui/button";
+import { OrganizationSwitcher } from "@clerk/nextjs";
 
 export const Navbar = () => {
     return (
-        <div className='fixed z-50 top-0 w-full h-14 border-b shadow-sm bg-white flex items-center'>
+        <nav className='fixed z-50 top-0 w-full px-4 h-14 border-b shadow-sm bg-white flex items-center'>
             <div className='flex items-center gap-x-4'>
-                <div className='hidden md:flex '>
+                <div className='hidden md:flex'>
                     <Logo />
                 </div>
+                <Button
+                    size='sm'
+                    className='rounded-sm hidden md:block h-auto py-1.5 px-2'
+                >
+                    Create
+                </Button>
+                <Button size='sm' className='rounded-sm block md:hidden'>
+                    <Plus className='h-4 w-4' />
+                </Button>
             </div>
-        </div>
+            <div className='ml-auto flex items-center gap-x-2'>
+                <OrganizationSwitcher
+                    hidePersonal
+                    afterCreateOrganizationUrl='/organization/:id'
+                    afterLeaveOrganizationUrl='/select-org'
+                    afterSelectOrganizationUrl='/organization/:id'
+                />
+            </div>
+        </nav>
     );
 };
