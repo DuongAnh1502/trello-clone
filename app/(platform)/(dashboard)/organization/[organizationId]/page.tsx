7b9b19d34b3b2e047ff062398ba187b96c0fe6ee
duonganh203 <rankill52@gmail.com>
2024-01-01 21:50:02 +0700

    adding button for organizationid page

--- app/(platform)/(dashboard)/organization/[organizationId]/page.tsx
+++ app/(platform)/(dashboard)/organization/[organizationId]/page.tsx
@@ -1,8 +1,18 @@
+import { create } from "@/actions/create-board";
+import { Button } from "@/components/ui/button";
+
 const OrganizationIdPage = () => {
     return (
         <div>
-            <form>
-                <input id='title' name='title' required />
+            <form action={create}>
+                <input
+                    id='title'
+                    name='title'
+                    required
+                    placeholder='Enter a board title'
+                    className='border-black border p-1'
+                />
+                <Button type='submit'>Submit</Button>
             </form>
         </div>
     );
