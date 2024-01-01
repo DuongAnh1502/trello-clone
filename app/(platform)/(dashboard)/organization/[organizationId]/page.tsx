beb08a761c1516122bbcaaf9849fec53f51ef3b3
duonganh203 <rankill52@gmail.com>
2024-01-01 22:45:06 +0700

    configure createboard

--- app/(platform)/(dashboard)/organization/[organizationId]/page.tsx
+++ app/(platform)/(dashboard)/organization/[organizationId]/page.tsx
@@ -1,19 +1,16 @@
-import { create } from "@/actions/create-board";
-import { Button } from "@/components/ui/button";
+import { db } from "@/lib/db";
+import { Form } from "./_components/form";
 
-const OrganizationIdPage = () => {
+const OrganizationIdPage = async () => {
+    const boards = await db.board.findMany();
     return (
-        <div>
-            <form action={create}>
-                <input
-                    id='title'
-                    name='title'
-                    required
-                    placeholder='Enter a board title'
-                    className='border-black border p-1'
-                />
-                <Button type='submit'>Submit</Button>
-            </form>
+        <div className='flex flex-col space-y-4'>
+            <Form />
+            <div className='space-y-2'>
+                {boards.map((board) => (
+                    <div key={board.id}>Board name : {board.title}</div>
+                ))}
+            </div>
         </div>
     );
 };
