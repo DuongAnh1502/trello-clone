112a4f31e371868909c40ad8bb65f8460a6cab53
duonganh203 <rankill52@gmail.com>
2024-01-20 16:38:25 +0700

    add info.tsx and skeleton for organization dashboard

--- app/(platform)/(dashboard)/organization/[organizationId]/page.tsx
+++ app/(platform)/(dashboard)/organization/[organizationId]/page.tsx
@@ -1,16 +1,9 @@
-import { db } from "@/lib/db";
-import { Form } from "./_components/form";
+import { Info } from "./_components/info";
 
 const OrganizationIdPage = async () => {
-    const boards = await db.board.findMany();
     return (
-        <div className='flex flex-col space-y-4'>
-            <Form />
-            <div className='space-y-2'>
-                {boards.map((board) => (
-                    <div key={board.id}>Board name : {board.title}</div>
-                ))}
-            </div>
+        <div className='w-full mb-20'>
+            <Info />
         </div>
     );
 };
