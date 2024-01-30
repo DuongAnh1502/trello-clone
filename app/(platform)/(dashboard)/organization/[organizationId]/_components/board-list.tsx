3b709f5536726cc2bdefab08260ccb52303e8f65
duonganh203 <rankill52@gmail.com>
2024-01-30 20:50:16 +0700

    remove '.'

--- app/(platform)/(dashboard)/organization/[organizationId]/_components/board-list.tsx
+++ app/(platform)/(dashboard)/organization/[organizationId]/_components/board-list.tsx
@@ -17,7 +17,7 @@ export const BoardList = () => {
                     <span className='text-xs'>5 remaining</span>
                     <Hint
                         sideOffset={40}
-                        description={`Free Workspaces can have up to 5 open boards. For unlimited boards upgrade this workspace.`}
+                        description={`Free Workspaces can have up to 5 open boards. For unlimited boards upgrade this workspace`}
                     >
                         <HelpCircle className='absolute bottom-2 right-2 h-[14px] w-[14px]' />
                     </Hint>
